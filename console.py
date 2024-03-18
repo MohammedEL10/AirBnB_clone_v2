681d11845bb9e39242d115a8f86e7265aae0504e
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-18 15:54:16 -0700

    AirBnB_clone_v2

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -274,7 +274,7 @@ class HBNBCommand(cmd.Cmd):
             args = args[2]
             if args and args[0] is '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
-                att_name = args[1:second_quote]
+                att_name = args[1:second_quo==]
                 args = args[second_quote + 1:]
 
             args = args.partition(' ')
