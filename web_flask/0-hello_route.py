1298c5cbceb1f4d1c6c1f887be5bd25821e396a1
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-19 11:14:59 -0700

    web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """script that starts a Flask web application:"""
-
-""" import the module flask """
 from flask import Flask
+
 """ the app take the model (__name__)"""
+
+
 app = Flask(__name__)
+
+
 @app.route("/")
 """ define Hello_HBNB """
+
+
 def Hello_HBNB():
     return "Hello, HBNB"
