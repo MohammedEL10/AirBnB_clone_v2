cf2d5f76aab5c96162b46ddb46f2d761e000dd11
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-19 16:33:18 -0700

    web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,9 +4,13 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
 @app.route('/hbnb/', strict_slashes=False)
 def HBNB():
     """return some text"""
     return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
