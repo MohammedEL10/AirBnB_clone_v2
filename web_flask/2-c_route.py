551c64e6c7c1b9881ad74e79b67853d1a0707f48
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-19 17:13:40 -0700

    web_flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,13 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hbnb/', strict_slashes=False)
-def HBNB():
-
-
-    """return some text"""
-    return "HBNB"
-@app.route(/c/<text>, strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def text():
     
 
