8af9db313d4f3bc672a5fdb16d267d3365d0be0e
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-19 17:25:41 -0700

    web_flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,16 +5,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/hbnb/', strict_slashes=False)
 def HBNB():
 
     """return some text"""
     return "HBNB"
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def text():
-    
 
     """return some text"""
     return "C"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
