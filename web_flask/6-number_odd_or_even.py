72b72dab7ddc05cd3000031fc15666d3ac31d1b2
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-21 11:30:58 -0700

    web_flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """script that starts a Flask web application:"""
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes=False
 
 
 @app.route('/')
